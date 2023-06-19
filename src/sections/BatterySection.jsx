a1335754b897a954076809fdb0c061a6781c1200
Dingchao <dingchaodeng@gatech.edu>
2023-06-19 09:46:29 +0800

    btr

--- src/sections/BatterySection.jsx
+++ src/sections/BatterySection.jsx
@@ -15,55 +15,77 @@ const Section = styled.section`
   background-color: var(--white);
 `;
 const Title = styled.h1`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-55%,-50%);
-    text-transform: uppercase;
-    font-size: var(--fontBig);
-    z-index: 1;
-
-`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-55%, -50%);
+  text-transform: uppercase;
+  font-size: var(--fontBig);
+  z-index: 1;
+`;
 
 const Battery = styled.ul`
-    position: absolute;
-    right: 4rem;
-    list-style: none;
-    background-color: var(--white);
-    border: 3px solid var(--dark);
-    border-radius: 8px;
-    padding: 0.5rem;
-    width: 15rem;
-    li{
-        width: 100%;
-        height: 5rem;
-        background-color: var(--dark);
-        background-image: linear-gradient(-90deg, var(--gradient));
-        
-
-    }
-    &>*:not(:first-child):not(:last-child){
-
-        margin: 0.5rem 0;
-    }
-`
-
+  position: absolute;
+  right: 4rem;
+  list-style: none;
+  background-color: var(--white);
+  border: 3px solid var(--dark);
+  border-radius: 8px;
+  padding: 0.5rem;
+  width: 15rem;
+  li {
+    width: 100%;
+    height: 5rem;
+    background-color: var(--dark);
+    background-image: linear-gradient(-90deg, var(--gradient));
+    opacity: 0;
+  }
+  & > *:not(:first-child):not(:last-child) {
+    margin: 0.5rem 0;
+  }
+`;
 
 const BatterySection = () => {
-    const battery = useRef(null);
-    
+  const battery = useRef(null);
+  let elements = gsap.utils.selector(battery)
+  useLayoutEffect(() => {
+    let t1 = gsap.timeline({
+    });
+    elements("li").forEach((el) => {
+      t1.to(el, {
+        scrollTrigger: {
+          trigger: el,
+          start: "top center",
+          end: "bottom center",
+          scrub: true,
+          markers: true,
+        },
+        opacity:1,
+      });
+    });
+    return () => {
+      t1.kill();
+    }
+
+        
+      
+  }
+  ,[])
+
+
+
   return (
     <Section id="battery">
-    <Title>Go all day with single charge...</Title>
-    <Battery ref={battery}>
-      <li />
-      <li />
-      <li />
-      <li />
-      <li />
-    </Battery>
-  </Section>
-  )
-}
+      <Title>Go all day with single charge...</Title>
+      <Battery ref={battery}>
+        <li />
+        <li />
+        <li />
+        <li />
+        <li />
+      </Battery>
+    </Section>
+  );
+};
 
-export default BatterySection
+export default BatterySection;
---
+++ src/sections/ColorSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ColorSection = () => {
+  return (
+    <div>ColorSection</div>
+  )
+}
+
+export default ColorSection
