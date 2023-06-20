1b20f9a54c4db6612f5e493aea1dff018fae2995
Dingchao <dingchaodeng@gatech.edu>
2023-06-20 14:33:08 +0800

    pricing

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Processor from "./sections/ProcessorSection";
 import BatterySection from "./sections/BatterySection";
 import ColorSection from "./sections/ColorSection";
 import CameraSection from "./sections/CameraSection";
+import PriceSection from "./sections/PriceSection";
 function App() {
   return (
     <>
@@ -21,6 +22,7 @@ function App() {
       <BatterySection />
       <ColorSection/>
       <CameraSection />
+      <PriceSection/>
     </>
   );
 }
